f9b706c9d7eeb73dc83b0c58c49e3f7ee8159d65
Elijah Drakeford <elijahdrakeford12@gmail.com>
2026-05-08 01:23:48 -0400

    init

---
+++ Lab16_edrakeford-1.py
@@ -0,0 +1,12 @@
+"""
+Program Name: Lab16_edrakeford-1.py
+
+Author: Elijah Drakeford
+
+Purpose: This program that reads the OHRU.csv file, which contains Ohio's unemployment rate since 1976.
+and parses the data to create a time-series line plot using matplotlib.
+
+Starter Code: None
+
+Date: May 8, 2026
+"""
